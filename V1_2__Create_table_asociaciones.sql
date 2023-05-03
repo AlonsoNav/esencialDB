dca9ea4f751839d26816b5cc8171548bb6724abd
Alonso Navarro <alonsonc89@gmail.com>
2023-05-03 15:32:29 -0600

    V1_2

---
+++ V1_2__Create_table_asociaciones.sql
@@ -0,0 +1,17 @@
+USE [esencialDB]
+GO
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[asociaciones](
+	[asociacionId] [bigint] IDENTITY(1,1) NOT NULL,
+	[nombre] [nchar](30) NOT NULL,
+	[cuentaBancaria] [varchar](30) NOT NULL,
+	[direccionId] [bigint] NOT NULL,
+ CONSTRAINT [PK_asociaciones] PRIMARY KEY CLUSTERED
+(
+	[asociacionId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
