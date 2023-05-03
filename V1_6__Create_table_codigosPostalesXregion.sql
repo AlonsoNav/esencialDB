1f81e87b9466b64223ab713025213b4ab5236ad4
Alonso Navarro <alonsonc89@gmail.com>
2023-05-03 15:40:25 -0600

    V1_6

---
+++ V1_6__Create_table_codigosPostalesXregion.sql
@@ -0,0 +1,10 @@
+GO
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[codigosPostalesXregion](
+	[codigoPostalId] [bigint] NOT NULL,
+	[regionId] [int] NOT NULL
+) ON [PRIMARY]
+GO
