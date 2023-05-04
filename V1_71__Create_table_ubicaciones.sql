8fa69587d8132255636e6dde5fcfd57ba4a03715
Alonso Navarro <alonsonc89@gmail.com>
2023-05-03 19:08:53 -0600

    V1_71

---
+++ V1_71__Create_table_ubicaciones.sql
@@ -0,0 +1,16 @@
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[ubicaciones](
+	[ubicacionId] [bigint] NOT NULL,
+	[fecha] [timestamp] NOT NULL,
+	[geoLocation] [geography] NOT NULL,
+	[accesedFrom] [varchar](20) NOT NULL,
+	[checkSum] [varbinary](150) NOT NULL,
+ CONSTRAINT [PK_ubicaciones] PRIMARY KEY CLUSTERED
+(
+	[ubicacionId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
