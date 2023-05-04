6abd1d4618b5223600ed1ed0e10dd6d3419262b4
Dominic Casares Aguirre <dominiccasares10@gmail.com>
2023-05-04 16:28:49 -0600

    Prueba 2

--- R__ConsultaOptimizacion Prueba.sql
+++ R__ConsultaOptimizacion Prueba.sql
@@ -1,4 +1,4 @@
-SET STATISTICS TIME ON
+USE[esencialDB]
 GO
 SELECT 
 	-- Agregate Functions
@@ -7,9 +7,8 @@ SELECT
 FROM dbo.inventarioMateriales IM
 	LEFT JOIN dbo.inspectores INS on INS.inspectorId = IM.inspectorId	 -- Left Join
 	INNER JOIN dbo.logProducciones P on P.produccionId = IM.produccionId	 
-	INNER JOIN dbo.materiales M on M.materialId= IM.materialId		     
+	LEFT JOIN dbo.materiales M on M.materialId= IM.materialId		     
 WHERE 
-	IM.inventarioID < 10000 AND
 	INS.inspectorId > 1000 AND											 -- Where Primary Field
 	uniMedida = 'kg'													 -- Igualdad
 GROUP BY P.cantidad													     -- Group By
@@ -24,11 +23,23 @@ FROM dbo.inventarioMateriales IM
 	LEFT JOIN dbo.inspectores INS on INS.inspectorId = IM.inspectorId	 -- Left Join
 	INNER JOIN dbo.logProducciones P on P.produccionId = IM.produccionId	   
 WHERE 
-	IM.inventarioID < 10000 AND
 	P.cantidad > 100 AND												 -- Where Non Primary Field
 	INS.nombre != 'NombreIns:1'											 -- Desigualdad
 GROUP BY P.cantidad													     -- Group By
 HAVING SUM(P.cantidad) > 5											 
 ORDER BY SUM(P.cantidad) DESC										     -- Sort
-FOR JSON PATH															 -- For JSON
+FOR JSON PATH	
 
+ALTER TABLE inventarioMateriales
+ADD inventarioID BIGINT IDENTITY(1,1) NOT NULL PRIMARY KEY;
+
+ALTER TABLE dbo.inventarioMateriales DROP CONSTRAINT PK__inventar__237474651C46A5B2; 
+ALTER TABLE dbo.InventarioMateriales DROP COLUMN InventarioID
+
+CREATE CLUSTERED INDEX ix_produccionId ON dbo.inventarioMateriales (produccionId)
+
+-- Índice no agrupado en la tabla inventarioMateriales para la columna inspectorId
+CREATE NONCLUSTERED INDEX IX_inventarioMateriales_inspectorId ON dbo.inventarioMateriales (inspectorId);
+
+-- Índice no agrupado en la tabla inventarioMateriales para la columna produccionId
+CREATE NONCLUSTERED INDEX IX_inventarioMateriales_produccionId ON dbo.inventarioMateriales (produccionId);
