f5b1ad3995b26e80857487b329c62c0782d38817
Alonso Navarro <alonsonc89@gmail.com>
2023-05-03 19:00:18 -0600

    V1_64

---
+++ V1_64__Create_table_subproductores.sql
@@ -0,0 +1,15 @@
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[subproductores](
+	[subproductorId] [bigint] IDENTITY(1,1) NOT NULL,
+	[productorId] [int] NOT NULL,
+	[deleted] [bit] NOT NULL,
+	[direccionId] [bigint] NOT NULL,
+ CONSTRAINT [PK_productorXlocalidades] PRIMARY KEY CLUSTERED
+(
+	[subproductorId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
