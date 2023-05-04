502ee91561731a063ab05c678b56b72e742bd440
Alonso Navarro <alonsonc89@gmail.com>
2023-05-03 18:59:22 -0600

    V1_63

---
+++ V1_63__Create_table_sponsors.sql
@@ -0,0 +1,16 @@
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[sponsors](
+	[sponsorId] [bigint] IDENTITY(1,1) NOT NULL,
+	[regionId] [int] NOT NULL,
+	[productorId] [int] NOT NULL,
+	[porcentaje] [decimal](5, 2) NOT NULL,
+	[contratoId] [bigint] NOT NULL,
+ CONSTRAINT [PK_sponsors] PRIMARY KEY CLUSTERED 
+(
+	[sponsorId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
